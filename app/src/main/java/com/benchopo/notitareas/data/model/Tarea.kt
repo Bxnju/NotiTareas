3661af2a3bd25c33f93d056140323a62df703c5a
Bxnju <andres.benjumea@upb.edu.co>
2025-04-10 17:30:43 -0500

    Add completion status to Tarea data class

--- app/src/main/java/com/benchopo/notitareas/data/model/Tarea.kt
+++ app/src/main/java/com/benchopo/notitareas/data/model/Tarea.kt
@@ -1,9 +1,13 @@
 package com.benchopo.notitareas.data.model
 
+import java.util.UUID
+
 data class Tarea(
+    val id: String = UUID.randomUUID().toString(),
     val titulo: String,
     val descripcion: String,
     val fechaEntrega: String,
-    val materia: String
+    val materia: String,
+    var completada: Boolean = false
 )
 
