8d9400768092ab96046896ad2efdb38dea18bef9
Bxnju <andres.benjumea@upb.edu.co>
2025-04-11 16:00:45 -0500

    Add AppTitle composable for enhanced UI header with gradient background

---
+++ app/src/main/java/com/benchopo/notitareas/ui/components/AppTitle.kt
@@ -0,0 +1,38 @@
+package com.benchopo.notitareas.ui.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun AppTitle() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(
+                Brush.linearGradient(
+                    listOf(Color(0xFF4710EE), Color(0xFF9031CB))
+                ), shape = RoundedCornerShape(30.dp)
+            )
+            .padding(16.dp),
+        horizontalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = "NotiTareas 😎✔",
+            style = MaterialTheme.typography.headlineMedium,
+            color = Color.White,
+            fontWeight = FontWeight.Bold
+        )
+    }
+}
