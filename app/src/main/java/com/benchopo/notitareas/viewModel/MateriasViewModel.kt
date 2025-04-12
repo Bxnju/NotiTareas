6fc7da8c14bd853ac8422cbf0f3e6131f5796af9
Bxnju <andres.benjumea@upb.edu.co>
2025-04-12 17:27:22 -0500

    Add the idProfesor to the Materia data class when creating a new object.

--- app/src/main/java/com/benchopo/notitareas/viewModel/MateriasViewModel.kt
+++ app/src/main/java/com/benchopo/notitareas/viewModel/MateriasViewModel.kt
@@ -8,15 +8,15 @@ class MateriasViewModel : ViewModel() {
     private val _materias = mutableStateListOf<Materia>()
     val materias: List<Materia> get() = _materias
 
-    fun agregarMateria(nombre: String): String? {
+    fun agregarMateria(nombre: String, idProfesor: String): String? {
         if (nombre.length > 35) return "El nombre no puede tener más de 35 caracteres."
         if (_materias.any { it.nombre.equals(nombre, ignoreCase = true) }) {
             return "Ya existe una materia con ese nombre."
         }
         if (nombre.isBlank()) return "El nombre no puede estar vacío."
 
-        _materias.add(Materia(nombre = nombre))
-        return null // No hubo error
+        _materias.add(Materia(nombre = nombre, idProfesor = idProfesor))
+        return null
     }
 
     fun eliminarMateria(id: String) {
