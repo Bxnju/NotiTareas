60e22145530984f36480df65063877523efbd113
Bxnju <andres.benjumea@upb.edu.co>
2025-04-13 15:56:01 -0500

    Refactor AuthViewModel to handle user login and registration
    -   Implement `login` function to search for an existing user by name and role in the `UsuariosViewModel` and set the `usuarioActual` if found.
    -   Implement `register` function to check if a user already exists and to add a new user to `UsuariosViewModel`.
    -   Update `login` and `register` to return error messages when applicable.

--- app/src/main/java/com/benchopo/notitareas/viewModel/AuthViewModel.kt
+++ app/src/main/java/com/benchopo/notitareas/viewModel/AuthViewModel.kt
@@ -11,8 +11,31 @@ class AuthViewModel : ViewModel() {
     var usuarioActual by mutableStateOf<Usuario?>(null)
         private set
 
-    fun login(nombre: String, rol: Rol) {
-        usuarioActual = Usuario(nombre = nombre, rol = rol)
+    fun login(nombre: String, rol: Rol, usuariosViewModel: UsuariosViewModel): String? {
+        // Buscar si ya existe el usuario
+        val existente = usuariosViewModel.usuarios.find { it.nombre == nombre && it.rol == rol }
+
+        if (existente == null) {
+            return "No existe un usuario con ese nombre y rol."
+        }else{
+            usuarioActual = existente
+            return null
+        }
+
+    }
+
+    fun register(nombre: String, rol: Rol, usuariosViewModel: UsuariosViewModel): String? {
+        // Buscar si ya existe el usuario
+        val existente = usuariosViewModel.usuarios.find { it.nombre == nombre && it.rol == rol }
+
+        if (existente != null) {
+            return "Ya existe un usuario con ese nombre y rol."
+        }else{
+            usuarioActual = Usuario(nombre = nombre, rol = rol).also {
+                usuariosViewModel.agregarUsuario(it)
+            }
+            return null
+        }
     }
 
     fun logout() {
