f6edb251c3ea17a9f6bfb80d81ab225c1bda4d6e
Bxnju <andres.benjumea@upb.edu.co>
2025-04-12 19:16:23 -0500

    Update color palette to adjust PurplePrimary and PurpleSecondary shades

--- app/src/main/java/com/benchopo/notitareas/ui/theme/Color.kt
+++ app/src/main/java/com/benchopo/notitareas/ui/theme/Color.kt
@@ -2,8 +2,8 @@ package com.benchopo.notitareas.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val PurplePrimary = Color(0xFF783AFF)
-val PurpleSecondary = Color(0xFF6C48BB)
+val PurplePrimary = Color(0xFF7E51E7)
+val PurpleSecondary = Color(0xFF67519B)
 val VibrantRed = Color(0xFFB61643)
 
 val PurplePrimaryLight = Color(0xFF783AFF)
