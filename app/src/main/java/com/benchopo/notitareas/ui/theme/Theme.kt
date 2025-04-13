a7b2394af04a03e09386c12f4d3d9609e1850f7e
Bxnju <andres.benjumea@upb.edu.co>
2025-04-12 19:16:32 -0500

    Update theme colors to match Material Design guidelines. Set onPrimary, onSecondary, and onTertiary to white.

--- app/src/main/java/com/benchopo/notitareas/ui/theme/Theme.kt
+++ app/src/main/java/com/benchopo/notitareas/ui/theme/Theme.kt
@@ -8,18 +8,25 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
     primary = PurplePrimary,
     secondary = PurpleSecondary,
-    tertiary = VibrantRed
+    tertiary = VibrantRed,
+    onPrimary = Color.White,
+    onSecondary = Color.White,
+    onTertiary = Color.White,
 )
 
 private val LightColorScheme = lightColorScheme(
     primary = PurplePrimaryLight,
     secondary = PurpleSecondaryLight,
-    tertiary = VibrantRedLight
+    tertiary = VibrantRedLight,
+    onPrimary = Color.White,
+    onSecondary = Color.White,
+    onTertiary = Color.White,
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
