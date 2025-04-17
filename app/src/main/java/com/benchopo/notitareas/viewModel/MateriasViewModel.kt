1aa27bb1edaf7af0cd1ea3758bab77104bdcddde
Bxnju <andres.benjumea@upb.edu.co>
2025-04-17 01:02:11 -0500

    Update MateriasViewModel to add a list of students to Materia data class

--- app/src/main/java/com/benchopo/notitareas/viewModel/MateriasViewModel.kt
+++ app/src/main/java/com/benchopo/notitareas/viewModel/MateriasViewModel.kt
@@ -8,14 +8,14 @@ class MateriasViewModel : ViewModel() {
     private val _materias = mutableStateListOf<Materia>()
     val materias: List<Materia> get() = _materias
 
-    fun agregarMateria(titulo: String, idProfesor: String): String? {
+    fun agregarMateria(titulo: String, idProfesor: String, estudiantes: MutableList<String>): String? {
         if (titulo.length > 35) return "El titulo no puede tener más de 35 caracteres."
         if (_materias.any { it.titulo.equals(titulo, ignoreCase = true) }) {
             return "Ya existe una materia con ese titulo."
         }
         if (titulo.isBlank()) return "El titulo no puede estar vacío."
 
-        _materias.add(Materia(titulo = titulo, idProfesor = idProfesor))
+        _materias.add(Materia(titulo = titulo, idProfesor = idProfesor, idEstudiantesInscritos = estudiantes ))
         return null
     }
 
