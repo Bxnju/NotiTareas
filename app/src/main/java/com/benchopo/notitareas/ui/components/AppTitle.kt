225cb4e0dc89eb205ba95726a30b90c68b7a2ffe
Bxnju <andres.benjumea@upb.edu.co>
2025-04-21 21:46:26 -0500

    Add logout button to AppTitle component and navigation to login screen
    The logout button was added to the AppTitle component, allowing users to log out and navigate back to the login screen.

--- app/src/main/java/com/benchopo/notitareas/ui/components/AppTitle.kt
+++ app/src/main/java/com/benchopo/notitareas/ui/components/AppTitle.kt
@@ -7,6 +7,8 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -15,10 +17,17 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
 import com.benchopo.notitareas.R
+import com.benchopo.notitareas.ui.navigation.Routes
+import com.benchopo.notitareas.viewModel.AuthViewModel
 
 @Composable
-fun AppTitle(usuarioNombre: String) {
+fun AppTitle(
+    navController: NavController,
+    authViewModel: AuthViewModel,
+    usuarioNombre: String
+) {
     Column(
         modifier = Modifier
             .fillMaxWidth()
@@ -27,7 +36,7 @@ fun AppTitle(usuarioNombre: String) {
         horizontalAlignment = androidx.compose.ui.Alignment.CenterHorizontally
     ) {
 
-        Row (
+        Row(
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(16.dp),
@@ -48,6 +57,22 @@ fun AppTitle(usuarioNombre: String) {
                 color = Color.White,
                 fontWeight = FontWeight.Bold
             )
+
+            //Boton de logout
+
+            Button(
+                onClick = {
+                    navController.navigate(Routes.Login){
+                        popUpTo(0) { inclusive = true }
+                    }
+                },
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = MaterialTheme.colorScheme.primary,
+                    contentColor = Color.White
+                )
+            ) {
+                Text("Cerrar sesión")
+            }
         }
 
     }
