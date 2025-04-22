a7026796f1c2cc2740cecbba3aab40eebff5135c
Bxnju <andres.benjumea@upb.edu.co>
2025-04-21 19:00:08 -0500

    Add email and password properties to Usuario data class.

--- app/src/main/java/com/benchopo/notitareas/data/model/Usuario.kt
+++ app/src/main/java/com/benchopo/notitareas/data/model/Usuario.kt
@@ -3,6 +3,8 @@ package com.benchopo.notitareas.data.model
 data class Usuario(
     val id: String,
     val nombre: String,
+    val email: String = "",
+    val password: String = "",
     val rol: Rol
 )
 
