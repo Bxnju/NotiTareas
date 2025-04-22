e11750ccd931474af4cbab4491aefe11f4f48b50
Bxnju <andres.benjumea@upb.edu.co>
2025-04-21 21:25:53 -0500

    Refactor TareasScreen to remove unused imports

--- app/src/main/java/com/benchopo/notitareas/ui/screens/tareas/TareasScreen.kt
+++ app/src/main/java/com/benchopo/notitareas/ui/screens/tareas/TareasScreen.kt
@@ -15,7 +15,6 @@ import androidx.compose.material3.HorizontalDivider
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
