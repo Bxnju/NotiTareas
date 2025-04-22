b66306c0f143e480ff0d5baf7cfed09e0a1e0ea1
Bxnju <andres.benjumea@upb.edu.co>
2025-04-21 21:03:40 -0500

    Update AppTitle composable to include the app logo and adjust the layout and styling

--- app/src/main/java/com/benchopo/notitareas/ui/components/AppTitle.kt
+++ app/src/main/java/com/benchopo/notitareas/ui/components/AppTitle.kt
@@ -1,11 +1,13 @@
 package com.benchopo.notitareas.ui.components
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -13,38 +15,43 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import com.benchopo.notitareas.R
 
 @Composable
 fun AppTitle(usuarioNombre: String) {
     Column(
         modifier = Modifier
             .fillMaxWidth()
-            .background(
-                Brush.linearGradient(
-                    listOf(Color(0xFF4710EE), Color(0xFF9031CB))
-                ), shape = RoundedCornerShape(30.dp)
-            )
             .padding(16.dp),
         verticalArrangement = Arrangement.spacedBy(5.dp),
         horizontalAlignment = androidx.compose.ui.Alignment.CenterHorizontally
     ) {
-       Row {
-           Text(
-               text = "NotiTareas",
-               style = MaterialTheme.typography.headlineMedium,
-               color = Color.White,
-               fontWeight = FontWeight.Bold
-           )
-       }
 
+        Row (
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            horizontalArrangement = Arrangement.SpaceAround,
+            verticalAlignment = androidx.compose.ui.Alignment.CenterVertically
+        ) {
 
-        Text(
-            text = "Bienvenido $usuarioNombre",
-            style = MaterialTheme.typography.bodySmall,
-            color = Color.White,
-            fontWeight = FontWeight.Bold
-        )
+            Image(
+                painter = painterResource(id = R.drawable.icon),
+                contentDescription = "Logo NotiTareas",
+                modifier = Modifier
+                    .size(50.dp)
+            )
+
+            Text(
+                text = "Bienvenido $usuarioNombre",
+                style = MaterialTheme.typography.bodyMedium,
+                color = Color.White,
+                fontWeight = FontWeight.Bold
+            )
+        }
+
     }
 }
